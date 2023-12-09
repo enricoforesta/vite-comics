0729df9fa82912a152eaa03a739571f5f951e147
enricoforesta <enrico.foresta97@gmail.com>
2023-12-09 15:23:12 +0100

    feat: componentBuy

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <script>
 import AppComponentNav from './components/ComponentHeader/AppComponentNav.vue';
 import AppComponentContent from './components/ComponentHeader/AppComponentContent.vue';
+import AppComponentBuy from './components/ComponetnMain/AppComponentBuy.vue';
 export default {
   name: "APP",
   components: {
     AppComponentNav,
     AppComponentContent,
-
+    AppComponentBuy,
   }
 }
 </script>
@@ -16,6 +17,9 @@ export default {
     <AppComponentNav />
     <AppComponentContent />
   </header>
+  <main>
+    <AppComponentBuy />
+  </main>
 </template>
 
 <style lang="scss">
---
+++ src/components/ComponetnMain/AppComponentBuy.vue
@@ -0,0 +1,75 @@
+<script>
+export default {
+    name: "Buy",
+    data() {
+        return {
+            buyContents: [
+                {
+                    title: "Digital Comics",
+                    path: "../../assets/img/buy-comics-digital-comics.png"
+                },
+                {
+                    title: "DC Merchandise",
+                    path: "../../assets/img/buy-comics-merchandise.png"
+                },
+                {
+                    title: "Subscription",
+                    path: "../../assets/img/buy-comics-subscriptions.png"
+                },
+                {
+                    title: "Comic Shop Locator",
+                    path: "../../assets/img/buy-comics-shop-locator.png"
+                },
+                {
+                    title: "DC Power",
+                    path: "../../assets/img/buy-dc-power-visa.svg"
+                },
+            ]
+        }
+    }
+}
+</script>
+<template>
+    <div class="buy">
+        <ul>
+            <li v-for='content in buyContents'>
+                <div class="image">
+                    <img src="../../assets/img/buy-dc-power-visa.svg" alt="img">
+                </div>
+                <div class="content"> {{ content.title }}
+                </div>
+            </li>
+        </ul>
+    </div>
+</template>
+<style scoped lang="scss">
+@use "../../style/variable.scss" as *;
+
+div.buy {
+    background-color: $color-bar;
+    @include padding-standard;
+
+    ul {
+        display: flex;
+        justify-content: space-between;
+        padding: 40px 0;
+        color: white;
+        gap: 10px;
+
+        li {
+            display: flex;
+            align-items: center;
+            gap: 10px;
+
+            div.image {
+                width: 45px;
+                display: flex;
+
+                img {
+                    width: 100%;
+                }
+            }
+        }
+    }
+}
+</style>
